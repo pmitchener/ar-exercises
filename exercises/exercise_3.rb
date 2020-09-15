690cc7d5473325047f4740768756ecbd2dbb9e9e
Patrick Mitchener <pmitchener@rogers.com>
2020-09-15 16:52:44 +0000

    Adding completed exercise_3.rb file

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,13 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
+require_relative '../lib/store'
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(name: "Gastown")
+@store3.destroy
+
+puts Store.count
