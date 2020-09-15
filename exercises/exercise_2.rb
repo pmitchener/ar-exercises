64a641c2a8b4159723e7fe697fecf32e76bcd250
Patrick Mitchener <pmitchener@rogers.com>
2020-09-15 16:48:59 +0000

    Adding completed exercise_2.rb file

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require_relative '../lib/store'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(name: "Burnaby")
+@store2 = Store.find_by(name: "Richmond")
+
+@store1.update(annual_revenue: 500000)
