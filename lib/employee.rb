cf16f96a66359fd8bbfc8c98de68b8a8ad09835e
Patrick Mitchener <pmitchener@rogers.com>
2020-09-24 03:24:07 +0000

    Adding lib files. Forgot to add them previously

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: { message: "last name must be given" }
+  validates :hourly_rate, inclusion: {in: 20..200 , message:  "%{value} is not a valid hourly rate."  }
+  validates :store_id, presence:{ message: "This employee must belong to a store" }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  #custom valication
+  validate :store_must_care_at_least_one_apparel_line
+
+  def store_must_care_at_least_one_apparel_line
+    if !mens_apparel && !womens_apparel
+      errors.add(:at_least_one_apparel, "Store must cary at least one apparel line")
+    end
+  end
+
 end
